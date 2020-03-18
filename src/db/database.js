4c913164c2e63a190d9832e8e6c56fdc5de4256f
Yenying-Ho <a26782871@gmail.com>
2020-03-18 11:01:08 +0800

    Revert "member by Ho"
    This reverts commit 2d6a5a20a97d47908328442c9a22c96cb45d0af2.

--- src/db/database.js
+++ src/db/database.js
@@ -6,7 +6,7 @@ import mysql from 'mysql2'
 const connection = {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'root',
   database: 'pet_db',
   waitForConnections: true,
   connectionLimit: 10,
