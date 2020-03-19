6025fbb482fab72457264043e0c68f9f890e30a4
WU NAN QING <a1993112@gmail.com>
2020-03-19 14:45:28 +0800

    已pull主幹

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import db from './db/database.js'
 //import 資料表 from './api/資料表.js'
 import product from './api/product.js'
 import member from './api/member.js'
-
-import serviceUser from './api/service_user.js'
+// import serviceUser from './api/service_user.js'
 
 const app = express()
 
@@ -36,8 +35,7 @@ app.use(bodyparser.urlencoded({ extended: false }))
 //app.use('/資料表', 資料表)
 app.use('/products', product)
 app.use('/member', member)
-
-app.use('/service_user', serviceUser)
+// app.use('/service_user', serviceUser)
 
 // 未找到的錯誤 - 404
 app.use((req, res, next) => {
